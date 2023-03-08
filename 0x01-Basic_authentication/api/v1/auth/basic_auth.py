1084afb479808eff6a08153fc0ee51f3dc966d15
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-03-08 17:28:07 +0100

    added function to extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -38,7 +38,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(self,
                                  decoded_base64_authorization_header:
                                  str) -> (str, str):
-        """ """
+        """ Extract user credentials from decoded header """
         if decoded_base64_authorization_header is None:
             return (None, None)
         if not isinstance(decoded_base64_authorization_header, str):
