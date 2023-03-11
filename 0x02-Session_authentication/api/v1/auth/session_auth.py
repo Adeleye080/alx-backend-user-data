e5571d582b176f8d778309fc93aa9cd1d9d9f22e
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-03-11 07:20:24 +0100

    SessionAuth class to handle session authorization protocols.

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module handles SimpleAPI session Authorization. """
+from api.v1.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ Session Authorization protocol. """
