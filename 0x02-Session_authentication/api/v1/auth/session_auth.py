47182567e1351b95dff6481dc4d32cb2ead516b3
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-03-11 07:43:36 +0100

    added method that creates user session.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,30 @@
 #!/usr/bin/env python3
-""" Module handles SimpleAPI session Authorization. """
+"""
+Module handles SimpleAPI session Authorization.
+"""
 from api.v1.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
-    """ Session Authorization protocol. """
-    pass
+    """
+    Session Authorization protocol.
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create Session ID for user
+        params:
+            user_id (str): user's ID
+        Return:
+            - None if user_id is None or not a string
+            - Session ID (str)
+        """
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+        id = str(uuid4())
+        self.user_id_by_session_id[id] = user_id
+        return id
