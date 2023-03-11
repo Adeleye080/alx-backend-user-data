d3d2b8ef86258a45a8e32fd19d52a7a4d7fc968a
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-03-11 14:06:10 +0100

    check for the presence of cookie and authorization header before handling request.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,11 +37,13 @@ def bef_req() -> None:
         excluded_path = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
         if auth.require_auth(request.path, excluded_path):
-            if auth.authorization_header(request) is None:
-                abort(401, description="Unauthorized")
+            if auth.authorization_header(request) is None\
+                    and auth.session_cookie(request) is None:
+                abort(401)
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
 
