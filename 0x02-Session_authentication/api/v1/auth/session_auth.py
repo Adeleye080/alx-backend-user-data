a762e53782ed6f78242447022807a150a569318c
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2023-03-11 14:30:53 +0100

    Added "current_user" method to retrieve current user using session cookie.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,11 @@
 """
 Module handles SimpleAPI session Authorization.
 """
+from models.user import User
+
 from .auth import Auth
 from uuid import uuid4
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -42,3 +45,11 @@ class SessionAuth(Auth):
         if not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves current user using session cookie
+        """
+        if self.session_cookie(request) is not None:
+            usr_id = self.user_id_for_session_id(self.session_cookie(request))
+            return User.get(usr_id)
